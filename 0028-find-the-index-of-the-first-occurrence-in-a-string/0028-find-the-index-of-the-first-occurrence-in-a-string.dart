37bc61c03a9e2517934cfa793c185292cd1c9273
Nada A <148198518+Nadayunus@users.noreply.github.com>
2025-08-08 09:45:45 +0530

    Time: 1 ms (29.02%), Space: 147.7 MB (76.86%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int strStr(String haystack, String needle) {
+    if (needle.isEmpty) return 0; // Edge case
+
+    int hLength = haystack.length;
+    int nLength = needle.length;
+
+    for (int i = 0; i <= hLength - nLength; i++) {
+      if (haystack.substring(i, i + nLength) == needle) {
+        return i;
+      }
+    }
+
+    return -1; // Not found
+  }
+}
